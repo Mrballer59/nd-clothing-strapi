0b31c20c0686efae060e883bc14724a5abe136e7
Brandon Joseph <brandonjoseph7@hotmail.co.uk>
2023-04-13 15:44:41 +0200

    categories section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Slider />
       <FeaturedProducts type="featured" />
       <FeaturedProducts type="trending" />
+      <Categories />
     </div>
   );
 };

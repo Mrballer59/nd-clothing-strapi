e75ecda6cdb924d7dd4efc3619dc543d83aff4c9
Brandon Joseph <brandonjoseph7@hotmail.co.uk>
2023-04-16 19:14:55 +0200

    products section

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import "./Products.scss";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Products Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">Shoe</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={1} />
+            <label htmlFor="2">Skirt</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={1} />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filters by Price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={200} />
+            <span>200</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price (Lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name="price" />
+            <label htmlFor="asc">Price (Highest first)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img
+          alt="img"
+          className=""
+          src="https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
